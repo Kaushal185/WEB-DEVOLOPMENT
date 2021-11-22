aab87c51f690d80a98c501d260bd7bcc6a697091
Kaushal <kaushalkawade183@gmail.com>
2021-11-22 16:36:11 +0530

    changes done

--- JAVASCRIPT_/COMPLETE_BASIC/notes.js
+++ JAVASCRIPT_/COMPLETE_BASIC/notes.js
@@ -108,30 +108,33 @@ var arr = [1,2,"bablu",4,true];
 // console.log(mydate.getMinutes());
 // console.log(mydate.getSeconds());
 
+// typeof
+// console.log(typeof(true));
+
 //* 11) Dom manupliation
 
 //* a) getElementBy Id and ClassName
-let elem = document.getElementById('click')
-console.log(elem);
+// let elem = document.getElementById('click')
+// console.log(elem);
 
-let eleclass = document.getElementsByClassName('container') // gebc
-console.log(eleclass);
+// let eleclass = document.getElementsByClassName('container') // gebc
+// console.log(eleclass);
 
-eleclass[0].style.background = "pink"; // add css
-eleclass[0].classList.add("para-text") // add css class
-eleclass[1].classList.add("classbyjs") 
+// eleclass[0].style.background = "pink"; // add css
+// eleclass[0].classList.add("para-text") // add css class
+// eleclass[1].classList.add("classbyjs") 
 
-console.clear();
+// console.clear();
 // console.log(elem.innerHTML);
 // console.log(eleclass[1].innerText);
 
 //* b) find element by tag name
 // tn = document.getElementsByTagName('button')
-tn = document.getElementsByTagName('div')
-console.log(tn);
+// tn = document.getElementsByTagName('div')
+// console.log(tn);
 
 //* c) create ele and append
-createdElement = document.createElement('p')
-createdElement.innerText = "This para element created by JS"
-tn[0].appendChild(createdElement); // first div append child
+// createdElement = document.createElement('p')
+// createdElement.innerText = "This para element created by JS"
+// tn[0].appendChild(createdElement); // first div append child
 
